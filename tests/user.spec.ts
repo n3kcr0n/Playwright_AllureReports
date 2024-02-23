c169eb6210203d2020d8360d18b991dd90c32c26
Necron <ronronmacayaon@gmail.com>
2024-02-23 23:33:12 +0800

    test2

--- tests/user.spec.ts
+++ tests/user.spec.ts
@@ -1,7 +1,6 @@
 import { test } from '@playwright/test';
 import User from "../models/User";
 import RegistrationPage from "../page-objects/RegistrationPage";
-
 //TestData
 const testUser:User = new User();
 test.describe('User Registration SUITE',async ()=>{
