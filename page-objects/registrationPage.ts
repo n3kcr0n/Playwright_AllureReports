447af68256a3b751f6c9b0960860ad6298377ebb
Necron <ronronmacayaon@gmail.com>
2024-02-23 22:41:05 +0800

    fix

--- page-objects/registrationPage.ts
+++ page-objects/registrationPage.ts
@@ -1,34 +1,23 @@
 import {Page} from '@playwright/test';
 
-
-
 export default class RegistrationPage{
-    //Selectors
     readonly url:string = '/signup';
-    readonly firstNameTextBox:string = "input[data-testid='first-name']";
-    readonly lastNameTextBox:string = "input[data-testid='last-name']";
-    readonly emailTextBox:string = "input[data-testid='email']";
-    readonly passwordTextBox:string = "input[data-testid='password']";
-    readonly repeatPasswordTextBox:string = "input[data-testid='confirm-password']";
-    readonly submitBtn:string = "button[data-testid='submit']";
-    readonly welcomeTitle:string = "[data-testid=welcome]";
-
     //Methods
     public async load(page:Page){
-        await page.goto(process.env.QACART_BASEURL?.toString()+this.url);
+        await page.goto(process.env.QACART_BASEURL+this.url);
     }
 
     public async fillRegistrationForm(page:Page,firstName:string,lastName:string,email:string,password:string){
-        await page.locator(this.firstNameTextBox).fill(firstName);
-        await page.locator(this.lastNameTextBox).fill(lastName);
-        await page.locator(this.emailTextBox).fill(email);
-        await page.locator(this.passwordTextBox).fill(password);
-        await page.locator(this.repeatPasswordTextBox).fill(password);
+        await page.locator("input[data-testid='first-name']").fill(firstName);
+        await page.locator("input[data-testid='last-name']").fill(lastName);
+        await page.locator("input[data-testid='email']").fill(email);
+        await page.locator("input[data-testid='password']").fill(password);
+        await page.locator( "input[data-testid='confirm-password']").fill(password);
     }
     
     public async submitRegistration(page:Page){
-        await page.locator(this.submitBtn).click();
-        await page.locator(this.welcomeTitle).waitFor({state:'visible'});
+        await page.locator("button[data-testid='submit']").click();
+        await page.locator("[data-testid=welcome]").waitFor({state:'visible'});
     }
 
     public async registerUser(page:Page,firstName:string,lastName:string,email:string,password:string){
--- tests/user.spec.ts
+++ tests/user.spec.ts
@@ -2,7 +2,6 @@ import { test } from '@playwright/test';
 import User from "../models/User";
 import RegistrationPage from "../page-objects/RegistrationPage";
 
-
 //TestData
 const testUser:User = new User();
 test.describe('User Registration SUITE',async ()=>{
