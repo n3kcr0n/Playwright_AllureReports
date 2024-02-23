93bc688dda94da312fb7f6f1d6f4b4a22b581c9d
Necron <ronronmacayaon@gmail.com>
2024-02-23 23:59:33 +0800

    test5

--- tests/user.spec.ts
+++ tests/user.spec.ts
@@ -1,13 +1,14 @@
 import { test } from '@playwright/test';
 import User from "../models/User";
-import RegistrationPage from "../page-objects/RegistrationPage";
+import QaCartRegistrationPage from '../page-objects/qaRegistrationPage';
+
 //TestData
 const testUser:User = new User();
 test.describe('User Registration SUITE',async ()=>{
     test('TS01 _Able to register a user.', async({browser})=>{
         const context = await browser.newContext();
         const page = await context.newPage();
-        const registrationPage = new RegistrationPage();
+        const registrationPage = new QaCartRegistrationPage();
         await registrationPage.load(page);
         await registrationPage.registerUser(
             page,
