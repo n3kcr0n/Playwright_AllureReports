f0c9495c2b31bf4995f420341a89d72f8767dc92
Necron <ronronmacayaon@gmail.com>
2024-02-23 23:44:40 +0800

    test3

--- page-objects/registrationPage.ts
+++ page-objects/registrationPage.ts
@@ -3,11 +3,11 @@ import {Page} from '@playwright/test';
 export default class RegistrationPage{
     readonly url:string = '/signup';
     //Methods
-    public async load(page:Page){
+    load=async(page:Page)=>{
         await page.goto(process.env.QACART_BASEURL+this.url);
     }
 
-    public async fillRegistrationForm(page:Page,firstName:string,lastName:string,email:string,password:string){
+    fillRegistrationForm =async(page:Page,firstName:string,lastName:string,email:string,password:string)=>{
         await page.locator("input[data-testid='first-name']").fill(firstName);
         await page.locator("input[data-testid='last-name']").fill(lastName);
         await page.locator("input[data-testid='email']").fill(email);
@@ -15,12 +15,12 @@ export default class RegistrationPage{
         await page.locator( "input[data-testid='confirm-password']").fill(password);
     }
     
-    public async submitRegistration(page:Page){
+    submitRegistration =async(page:Page)=>{
         await page.locator("button[data-testid='submit']").click();
         await page.locator("[data-testid=welcome]").waitFor({state:'visible'});
     }
 
-    public async registerUser(page:Page,firstName:string,lastName:string,email:string,password:string){
+    registerUser =async (page:Page,firstName:string,lastName:string,email:string,password:string)=>{
         await this.fillRegistrationForm(page,firstName,lastName,email,password);
         await this.submitRegistration(page);
     }
