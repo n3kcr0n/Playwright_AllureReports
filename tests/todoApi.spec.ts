4c3221fb2e046934002cccea01b0c55431eccc34
Necron <ronronmacayaon@gmail.com>
2024-02-24 11:49:00 +0800

    fix api

--- tests/todoApi.spec.ts
+++ tests/todoApi.spec.ts
@@ -4,17 +4,17 @@ import UserApi from "../apis/userApi";
 
 //TestData
 const testUser1:User = new User();
-const testUser2:User = new User();
+test.describe.configure({ mode: 'serial' });
 test.describe('QA CART Create to do via API',async ()=>{
     test('TS01 _Able to register using API',async ({request,context})=>{
         const response =await new UserApi().signUpUserAPI(request,testUser1,context);
-        const responseBody = await response.json();
+        //const responseBody = await response.json();
+        expect(response.ok()).toBeTruthy(); 
         expect(response.status()).toEqual(201);
     });
 
-    test('TS03 _Able to create to do ',async ({request,context})=>{
-        await new UserApi().signUpUserAPI(request,testUser2,context);
-        const response = await new UserApi().createTodoAPI(request,'Playwright',testUser2);
+    test('TS03 _Able to create to do ',async ({request})=>{
+        const response = await new UserApi().createTodoAPI(request,'Playwright',testUser1);
         expect(response.status()).toEqual(201);
     });
 });
